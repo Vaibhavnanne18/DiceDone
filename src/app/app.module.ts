a6568ff7be5b6ce66c6bee4ad252d480b1e1e093
Vaibhavnanne18 <nannevaibhav@gmail.com>
2023-02-22 00:40:49 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { GamesComponent } from './games/games.component';
+import { Routes,RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AddplayerComponent } from './addplayer/addplayer.component';
+import { LastWinsComponent } from './last-wins/last-wins.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire/compat';
+import { AgentComponent } from './agent/agent.component';
+import { MembersInGameComponent } from './members-in-game/members-in-game.component';
+import { AgentListComponent } from './agent-list/agent-list.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { EditdisComponent } from './editdis/editdis.component';
+import { AddagentComponent } from './addagent/addagent.component';
+import { EditagentComponent } from './editagent/editagent.component';
+import { MemberdetailsComponent } from './memberdetails/memberdetails.component';
+import { WalletComponent } from './wallet/wallet.component';
+
+
+const appRouts :Routes=[
+{path:'games',component:GamesComponent},
+{path:'login',component:LoginComponent},
+{path:'add',component:AddplayerComponent},
+{path:'dis/:disId/addagent',component:AddagentComponent},
+{path:'last-matches/:id',component:LastWinsComponent},
+{path:'dis/:disId/agent',component:AgentComponent},
+{path:'dis/:disId/agent/:agentId',component:EditagentComponent},
+{path:'members',component:AgentComponent},
+{path:'wallet',component:WalletComponent},
+{path:'',component:HomeComponent},
+{path:'',component:AgentListComponent},
+{path:'dis/:id',component:EditdisComponent},
+{path:'member/:member_id',component:MemberdetailsComponent},
+
+// {path:'dis/:id/agent/:agentid',component:EditdisComponent},
+// {path:'dis/:id/agent/:agentid/player/:playerid',component:EditdisComponent},
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    GamesComponent,
+    LoginComponent,
+    AddplayerComponent,
+    LastWinsComponent,
+    AgentComponent,
+    MembersInGameComponent,
+    AgentListComponent,
+    UserListComponent,
+
+    EditdisComponent,
+    AddagentComponent,
+    EditagentComponent,
+    MemberdetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRouts),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
